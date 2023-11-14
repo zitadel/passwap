5902cebe0a1f771aee29ed20fe60e1d0f7618b03
Tim Möhlmann <tim+github@zitadel.com>
2023-11-14 11:59:05 +0200

    chore(docs): write reference for encoding schemes
    Related to https://github.com/zitadel/zitadel/issues/6896

--- internal/encoding/pbkdf2.go
+++ internal/encoding/pbkdf2.go
@@ -8,7 +8,7 @@ import (
 const encodePbkdf2 = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789./"
 
 // Pbkdf2B64 is an alternative base64 encoding used by pbkdf2.
-// Bassicaly it's `+` replaced by `.`.
+// Basically it's `+` replaced by `.`.
 // https://passlib.readthedocs.io/en/stable/lib/passlib.utils.binary.html#passlib.utils.binary.ab64_encode
 var Pbkdf2B64 = base64.NewEncoding(encodePbkdf2).WithPadding(base64.NoPadding)
 
